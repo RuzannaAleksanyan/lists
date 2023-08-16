10d94847557a5463e6326ee329a9885f436c319d
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-08-17 00:05:14 +0400

    Create node.hpp

---
+++ linked_list/node.hpp
@@ -0,0 +1,17 @@
+#ifndef NODE
+#define NODE
+
+template <typename T>
+struct node
+{
+    node(const T& data = T{}, node* next = nullptr)
+        : m_data(data)
+        , m_next(next)
+    {
+    }
+
+    T m_data;
+    node* m_next;
+};
+
+#endif
